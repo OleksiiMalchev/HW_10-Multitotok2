85e4497ada65106da95e2abb7e7448db70def947
OleksiiMalchev <oleksiimalchev@gmail.com>
2022-11-01 14:41:49 +0200

    HW10 is done

---
+++ Main.java
@@ -0,0 +1,30 @@
+import com.sun.source.util.SourcePositions;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        PetrolStation petrolStation = new PetrolStation();
+        petrolStation.addFuelToStation(10000.0);
+        petrolStation.doRefuel(50.0);
+        petrolStation.doRefuel(75.5);
+        petrolStation.doRefuel(75.5);
+        petrolStation.doRefuel(75.5);
+        petrolStation.doRefuel(30.5);
+        petrolStation.doRefuel(30.5);
+        petrolStation.doRefuel(30.5);
+        petrolStation.doRefuel(25.0);
+        petrolStation.doRefuel(25.0);
+        petrolStation.doRefuel(250.0);
+        petrolStation.doRefuel(25.0);
+        petrolStation.doRefuel(251.0);
+        petrolStation.doRefuel(25.0);
+        petrolStation.doRefuel(55.0);
+        petrolStation.doRefuel(25.0);
+        petrolStation.doRefuel(81.0);
+
+
+
+
+    }
+}
---
+++ ThreadSafeList.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ThreadSafeList<T> {
+
+    private final List<T> safeList = new CopyOnWriteArrayList<>();
+
+
+    public boolean addToList(T e) {
+        Iterator<T> iterator = safeList.iterator();
+        return safeList.add(e);
+    }
+
+    public void remove(T remove) {
+        for (T e : safeList) {
+            if (e.equals(remove)) {
+                safeList.remove(e);
+            }
+        }
+    }
+
+    public T get(int index) {
+        return safeList.get(index);
+    }
+
+}
