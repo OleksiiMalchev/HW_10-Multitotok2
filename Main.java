1710e2ca09913ec425456f12589bff8785f8a79f
OleksiiMalchev <oleksiimalchev@gmail.com>
2022-11-03 18:25:11 +0200

    HW10 is done.Custom safeList

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
     public static void main(String[] args) {
         PetrolStation petrolStation = new PetrolStation();
@@ -18,5 +19,19 @@ public class Main {
         petrolStation.doRefuel(55.0);
         petrolStation.doRefuel(25.0);
         petrolStation.doRefuel(81.0);
+
+        ThreadSafeList<Integer> threadSafeList = new ThreadSafeList<>();
+        new Thread(()->threadSafeList.add(7)).start();
+        new Thread(()->threadSafeList.add(5)).start();
+        new Thread(()->threadSafeList.add(11)).start();
+        new Thread(()->threadSafeList.add(13)).start();
+//          new Thread(()->threadSafeList.remove(3)).start();
+//          new Thread(()->threadSafeList.remove(0)).start();
+        int size = threadSafeList.getSize();
+        System.out.println(size);
+        Integer integer = threadSafeList.get(0);
+        Integer integer1 = threadSafeList.get(2);
+        System.out.println(integer);
+        System.out.println(integer1);
     }
 }
--- ThreadSafeList.java
+++ ThreadSafeList.java
@@ -1,26 +1,75 @@
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+
+import java.util.Arrays;
+
 
 public class ThreadSafeList<T> {
+    final transient Object lock = new Object();
+    private T[] array;
+    private int size = 0;
 
-    private final List<T> safeList = new CopyOnWriteArrayList<>();
-
-
-    public boolean addToList(T e) {
-        Iterator<T> iterator = safeList.iterator();
-        return safeList.add(e);
+    public ThreadSafeList() {
+        setArray((T[]) new Object[0]);
     }
 
-    public void remove(T remove) {
-        for (T e : safeList) {
-            if (e.equals(remove)) {
-                safeList.remove(e);
-            }
+    private T[] getArray() {
+        return array;
+    }
+
+    private void setArray(T[] t) {
+        array = t;
+    }
+
+    public boolean add(T t) {
+        synchronized (lock) {
+            T[] extendedArray = getArray();
+            int length = extendedArray.length;
+            extendedArray = Arrays.copyOf(extendedArray, length + 1);
+            extendedArray[length] = t;
+            setArray(extendedArray);
+            System.out.println(Thread.currentThread().getName());
+            size++;
+            return true;
         }
     }
 
+    public T remove(int index) {
+        if (index > array.length - 1) {
+            throw new ArrayIndexOutOfBoundsException("Index is greater than array size");
+        }
+        T[] truncatedArray = getArray();
+        int length = truncatedArray.length;
+        T oldValue = truncatedArray[index];
+        int numMoved = length - index - 1;
+        T[] newArray;
+        if (numMoved == 0) {
+            newArray = Arrays.copyOf(truncatedArray, length - 1);
+            size--;
+        } else {
+            newArray = (T[]) new Object[length - 1];
+            System.arraycopy(truncatedArray, 0, newArray, 0, index);
+            System.arraycopy(truncatedArray, index + 1, newArray, index, numMoved);
+            size--;
+        }
+        setArray(newArray);
+        return oldValue;
+    }
+
     public T get(int index) {
-        return safeList.get(index);
+        if (index > array.length - 1) {
+            throw new ArrayIndexOutOfBoundsException("Index is greater than array size");
+        }
+        return array[index];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ThreadSafeList{" +
+                "array=" + Arrays.toString(array) +
+                '}';
     }
 }
